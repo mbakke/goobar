bbe9a53a579e541f0625f187eb18d2f814e3e014
Marius Bakke <marius@gnu.org>
2023-07-11 17:19:54 +0800

    Add license header to shell tests.
    * tests/goobar/test-config.sh, tests/goobar/test-output.sh: Add
    license header.

--- tests/goobar/test-config.sh
+++ tests/goobar/test-config.sh
@@ -1,3 +1,20 @@
+# This file is part of Goobar.
+#
+# Copyright © 2023 Marius Bakke
+#
+# Goobar is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Goobar. If not, see <https://www.gnu.org/licenses/>.
+
 # Ensure the default configuration works.
 goobar --one-shot
 
